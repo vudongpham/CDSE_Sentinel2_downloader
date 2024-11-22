f13fc35d25372930b6b78f6c395722d49fba5fc8
vudongpham <vudong.pham27@gmail.com>
2024-11-22 21:01:39 +0100

    update example

--- example_commands/download_command.sh
+++ example_commands/download_command.sh
@@ -4,7 +4,7 @@
 
 docker run -it --rm \
     -v $(pwd):$(pwd) \
-    cdse-s2 download \
-    $(pwd)/test_data/test.json \
-    $(pwd)/test_data/download_dir \
-    $(pwd)/test_data/test_data/secret.txt
+    vudongpham/cdse-s2 download \
+    $(pwd)/test_data/query_20241122T195540.json \
+    $(pwd)/download_dir \
+    $(pwd)/test_data/secret.txt
--- example_commands/search_command.sh
+++ example_commands/search_command.sh
@@ -2,7 +2,7 @@
 
 docker run -it --rm \
     -v $(pwd):$(pwd) \
-    cdse-s2 search\
-    --daterange 20170101,20171231 --cloudcover 0,75 \
-    $(pwd)/test_data/test_bound3.gpkg \
+    vudongpham/cdse-s2 search \
+    --daterange 20240101,20240630 --cloudcover 0,75 \
+    $(pwd)/test_data/berlin_bound.gpkg \
     $(pwd)/test_data
